cd7e2c4e8a6b0ffe17a4051e202f8ff6a335461f
HoangPhuc08 <phuchoangpham08@gmail.com>
2025-03-16 09:55:40 +0700

    update

---
+++ EEC_ICT.Data/Services/QuestionResults.cs
@@ -0,0 +1,41 @@
+﻿using EEC_ICT.Data.Core;
+using EEC_ICT.Data.Models;
+using EEC_ICT.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEC_ICT.Data.Services
+{
+    public class QuestionResultsServices
+    {
+        private static QuestionResultsRepository rep = new QuestionResultsRepository();
+
+        public static List<QuestionResults> SelectAll()
+        {
+            return SqlHelper.GetList<QuestionResults>(rep.SelectAll());
+        }
+
+        public static QuestionResults SelectOne(int questionId, string userId)
+        {
+            return SqlHelper.GetInfo<QuestionResults>(rep.SelectOne(questionId, userId));
+        }
+
+        public static string Insert(QuestionResults entity)
+        {
+            return rep.Insert(entity);
+        }
+
+        public static string Update(QuestionResults entity)
+        {
+            return rep.Update(entity);
+        }
+
+        public static string Delete(int questionId)
+        {
+            return rep.Delete(questionId);
+        }
+    }
+}
