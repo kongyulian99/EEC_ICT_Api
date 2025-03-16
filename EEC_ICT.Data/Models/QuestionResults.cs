16c29ed33d1c02e209de5dd937a15f5a428f496b
HoangPhuc08 <phuchoangpham08@gmail.com>
2025-03-16 09:12:53 +0700

    update

---
+++ EEC_ICT.Data/Models/QuestionResults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEC_ICT.Data.Models
+{
+    public class QuestionResults
+    {
+        public int QuestionId { get; set; }
+        public bool Result { get; set; }
+        public string UserId { get; set; }
+
+    }
+}
